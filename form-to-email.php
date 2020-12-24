3892fa7cefa2ce83e7e4a0a31034d3b443cbb5b7
Justin Ling <48165446+justinrling@users.noreply.github.com>
2020-12-24 15:06:37 -0800

    Create form-to-email.php

---
+++ form-to-email.php
@@ -0,0 +1,27 @@
+<?php
+if(!isset($_POST['submit']))
+{
+  echo "Error; you need to submit the form.";
+}
+
+$name = $_POST['fullname'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+//validate
+if(empty($name)||empty($email)){
+  echo "Name and email are required.";
+}
+
+$email_from = 'jling@scu.edu';
+$email_subject = "Website: New Form Submission";
+$email_body = "You have received a new message from: $name.\n"."email address: $email\n"."Message:\n $message".
+
+$to = "jling@scu.edu";
+$headers = "From: $email_from \r\n";
+
+//Send email
+mail($to,$email_subject,$email_body,$headers);
+//done
+
+?>
